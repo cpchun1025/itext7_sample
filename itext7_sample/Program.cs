b7c13f7e2d6e0dd0f35e346adb62e75c49ec2b07
Chris Auyeung <cpchun@gmail.com>
2024-09-25 22:56:58 +0800

    pp

--- itext7_sample/Program.cs
+++ itext7_sample/Program.cs
@@ -3,26 +3,76 @@ using System.IO;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using itext7_sample;
 
 class Program
 {
     static void Main(string[] args)
     {
         string pdfPath = "D:\\dev\\net\\itext7_sample\\Technical_spec_for_SD6_and_SD6A.pdf";
+        List<TableRow> tableData = new List<TableRow>();
 
         // Open the PDF document
         using (PdfReader pdfReader = new PdfReader(pdfPath))
         using (PdfDocument pdfDoc = new PdfDocument(pdfReader))
         {
-            for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
-            {
-                var strategy = new SimpleTextExtractionStrategy();
-                string pageText = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+            int lastPageNumber = pdfDoc.GetNumberOfPages();
+            var strategy = new SimpleTextExtractionStrategy();
+            string lastPageText = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(lastPageNumber), strategy);
 
-                Console.WriteLine($"Page {page}:");
-                Console.WriteLine(pageText);
-                Console.WriteLine();
+            // Parse the table from the last page's text
+            tableData = ParseTable(lastPageText);
+        }
+
+        // Output the parsed table data
+        foreach (var row in tableData)
+        {
+            Console.WriteLine($"{row.Date} {row.Time} {row.Code1} {row.Value1} ...");
+        }
+    }
+
+    // Method to parse the table from the extracted text
+    static List<TableRow> ParseTable(string text)
+    {
+        List<TableRow> tableRows = new List<TableRow>();
+
+        // Split the text by lines (each line represents a row in the table)
+        var lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var line in lines)
+        {
+            // Split each line by spaces (each column is separated by spaces)
+            var columns = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Ensure the line has enough columns before creating a TableRow object
+            if (columns.Length >= 18)
+            {
+                TableRow row = new TableRow
+                {
+                    Date = columns[0],
+                    Time = columns[1],
+                    Code1 = columns[2],
+                    Value1 = columns[3],
+                    Code2 = columns[4],
+                    Value2 = columns[5],
+                    Code3 = columns[6],
+                    Value3 = columns[7],
+                    Value4 = columns[8],
+                    Value5 = columns[9],
+                    LargeNumber = columns[10],
+                    Identifier1 = columns[11],
+                    Identifier2 = columns[12],
+                    Date1 = columns[13],
+                    Date2 = columns[14],
+                    Attachment = columns[15],
+                    Value6 = columns[16],
+                    Value7 = columns[17],
+                };
+
+                tableRows.Add(row);
             }
         }
+
+        return tableRows;
     }
 }
---
+++ itext7_sample/TableRow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itext7_sample
+{
+    public class TableRow
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Code1 { get; set; }
+        public string Value1 { get; set; }
+        public string Code2 { get; set; }
+        public string Value2 { get; set; }
+        public string Code3 { get; set; }
+        public string Value3 { get; set; }
+        public string Value4 { get; set; }
+        public string Value5 { get; set; }
+        public string LargeNumber { get; set; }
+        public string Identifier1 { get; set; }
+        public string Identifier2 { get; set; }
+        public string Date1 { get; set; }
+        public string Date2 { get; set; }
+        public string Attachment { get; set; }
+        public string Value6 { get; set; }
+        public string Value7 { get; set; }
+    }
+}
